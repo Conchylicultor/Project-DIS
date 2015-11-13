10dbb5258b24c9ec3b0035fbafd97ba1babfa740
Conchylicultor <etiennefg.pot@gmail.com>
2015-11-13 15:08:05 +0100

    Initial commit

---
+++ controllers/supervisor/supervisor.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+#include <webots/robot.h>
+#include <webots/emitter.h>
+#include <webots/supervisor.h>
+
+/*
+ * Initialize flock position and devices.
+ */
+void reset(void)
+{
+}
+
+
+/*
+ * Compute performance metric.
+ */
+void compute_fitness(void)
+{
+}
+
+
+
+/*
+ * Main function.
+ */
+ 
+int main(int argc, char *args[])
+{
+    return 0;
+}
---
+++ controllers/swarm/swarm.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+#include <webots/robot.h>
+
+#include <webots/differential_wheels.h>
+#include <webots/distance_sensor.h>
+#include <webots/emitter.h>
+
+
+int robot_id; // Unique robot ID
+
+static void reset(void)
+{
+}
+
+
+int main(int argc, char *args[])
+{
+    return 0;
+}  
+  
