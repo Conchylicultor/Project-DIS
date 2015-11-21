0ffcbedcc04b585bcc74f5c950d2516c2fa9737f
Marco Antognini <antognini.marco@gmail.com>
2015-11-21 13:52:28 +0100

    Add basic e-puck life-cycle code structure

--- controllers/swarm/swarm.cpp
+++ controllers/swarm/swarm.cpp
@@ -1,4 +1,8 @@
+#include <cassert>
+#include <cmath>
 #include <iostream>
+#include <string>
+#include <vector>
 
 #include <webots/robot.h>
 
@@ -7,15 +11,44 @@
 #include <webots/emitter.h>
 
 
+/*************************************************** Overall settings     *****/
+
+const int TIME_STEP = 32;
+
+
+/*************************************************** Robot's capabilities *****/
+
 int robot_id; // Unique robot ID
 
-static void reset(void)
+
+/*************************************************** Robot's setup        *****/
+
+void reset(void)
+{
+    char const* robot_name = wb_robot_get_name();
+    std::cout << "Reset of robot " << robot_name << std::endl;
+
+    sscanf(robot_name, "epuck%d", &robot_id);
+}
+
+/*************************************************** Robot's main algo    *****/
+
+void step()
 {
 }
 
-
-int main(int argc, char *args[])
+int main(int argc, char* args[])
 {
+    wb_robot_init();
+    reset();
+
+    // Loop until webots wants to stop
+    while (wb_robot_step(TIME_STEP) != -1) {
+        step();
+    }
+
+    wb_robot_cleanup();
+
     return 0;
-}  
-  
+}
+
