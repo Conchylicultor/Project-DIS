6208ebdaf05ed964451b3414744b5a35c2b3e672
Marco Antognini <antognini.marco@gmail.com>
2015-12-12 14:21:52 +0100

    Add ability to replay best settings

--- controllers/common/Config.hpp
+++ controllers/common/Config.hpp
@@ -5,6 +5,8 @@
  * Here are defined common settings of supervisor and swarm controllers
  */
 
+#define REPLAY 0 // if 1, only send best settings from PSO to e-pucks
+
 int const TIME_STEP = 64; // [ms] Length of time step
 double const DELTA_T = TIME_STEP/1000.0; // [s] Length of time step
 
